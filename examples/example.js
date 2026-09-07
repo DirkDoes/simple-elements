1d2acbb2741251a28674363e25f49b1b46587e9d
Dirk <dirkroosendaal04@gmail.com>
2026-09-07 17:07:21 +0200

    Refresh polished catalog deployment

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-import { componentCatalog } from './catalog.js?v=a39ed3b';
+import { componentCatalog } from './catalog.js?v=4706284';
 
 const titleFor = (tag) => tag.split('-').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
 const pages = componentCatalog.map(({ tag, icon }) => [tag, titleFor(tag), icon]);
