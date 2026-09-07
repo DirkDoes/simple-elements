d3fbd4a9def1b71c72b7a6ce2952b798d54ddfcc
Dirk <dirkroosendaal04@gmail.com>
2026-09-07 18:57:55 +0200

    Refresh sidebar pattern assets

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-import { componentCatalog, patternCatalog } from './catalog.js?v=8428e24';
+import { componentCatalog, patternCatalog } from './catalog.js?v=d5ddac1';
 
 const titleFor = (tag) => tag.split('-').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
 const componentPages = componentCatalog.map(({ tag, icon }) => [tag, titleFor(tag), icon]);
